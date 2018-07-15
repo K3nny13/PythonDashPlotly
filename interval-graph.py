1b6397a5f2630c0eb613823742346be77a8fe7ef
K3nny13 <kenneth_cogzell@hotmail.com>
2018-07-15 14:45:43 +0200

    Changed interval time to 3 seconds

--- interval-graph.py
+++ interval-graph.py
@@ -2,6 +2,7 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
+import plotly.graph_objs as go
 import requests
 
 app = dash.Dash()
@@ -11,18 +12,17 @@ data_url = 'https://data-live.flightradar24.com/zones/fcgi/feed.js?faa=1' \
 
 app.layout = html.Div([
                         html.Div([
-                            html.Iframe(src="https://www.flightradar24.com",
-                                        height=500,
-                                        width=1200)
-                        ]),
-                        html.Div([
-                            html.Pre(id='counter_text',
-                                     children="Active Flights Worldwide"),
+                            html.H1(id='counter_text',
+                                    style={'textAlign': 'center'}),
+                            dcc.Graph(id='live-update-graph',
+                                      style={'width': 1800}),
                             dcc.Interval(id='interval-components',
-                                         interval=100000,
+                                         interval=3000,
                                          n_intervals=0)
                         ])
 ])
+
+counter_list = []
 
 
 @app.callback(Output('counter_text', 'children'),
@@ -33,7 +33,19 @@ def update_layout(n):
     counter = 0
     for element in data['stats']['total']:
         counter += data['stats']['total'][element]
+    counter_list.append(counter)
     return "Active Flights Worldwide: {}".format(counter)
+
+
+@app.callback(Output('live-update-graph', 'figure'),
+              [Input('interval-components', 'n_intervals')])
+def update_graph(n):
+    fig = go.Figure(data=[
+                    go.Scatter(x=list(range(len(counter_list))),
+                               y=counter_list,
+                               mode='lines+markers')
+                    ])
+    return fig
 
 
 if __name__ == '__main__':
